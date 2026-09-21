2cf20430e5afb651fa522de68931875e76c9cba4
Samuel <castillo.samuel.702@gmail.com>
2026-09-21 17:02:15 -0500

    Dao de Casos

--- Entrevista/app/src/main/java/logic/DAOs.kt
+++ Entrevista/app/src/main/java/logic/DAOs.kt
@@ -1,4 +1,33 @@
 package logic
 
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface CasoDao{
+    //crear y guardar caso
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertarCaso(caso: Caso): Long
+    //Editar caso
+    @Update
+    suspend fun actualizarCaso(caso: Caso)
+    //Eliminar caso
+    @Delete
+    suspend fun eliminarCaso(caso: Caso)
+    //Lista de casos
+    @Query("SELECT * FROM casos ORDER BY id DESC")
+    fun obtenerCasos(): Flow<List<Caso>>
+    //Buscar caso por titulo o descripcion
+    @Query("SELECT * FROM casos WHERE titulo LIKE '%' || :busqueda || '%' OR descripcion LIKE '%' || :busqueda || '%'")
+    fun buscarCasos(busqueda: String): Flow<List<Caso>>
+    // Caso especifico al darle click
+    @Query("SELECT * FROM casos WHERE id = :id")
+    suspend fun obtenerCasoId(id: Int): Caso?
+}
 class DAOs {
 }
